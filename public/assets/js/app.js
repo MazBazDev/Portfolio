b1db463d2f57f6bc58190e9342a21237ad542538
MazBaz <mrlog42@gmail.com>
2023-04-02 10:37:04 +0200

    added light/dark switch

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -25,13 +25,37 @@ document.getElementById("scaleDown").addEventListener("click", function () {
     map.zoomTo(map.getZoom() - 1);
 });
 
-// Récupération de l'élément checkbox et de la balise body
-const checkbox = document.getElementById("check-theme");
-const body = document.querySelector("body");
-
 ScrollReveal().reveal(".card", {
     delay: 200,
     reset: true,
     origin: "bottom",
     duration: 1000,
+});
+
+const checkbox = document.getElementById('check-theme');
+const body = document.querySelector('body');
+
+// Récupérer la valeur actuelle du thème dans le localStorage
+const savedTheme = localStorage.getItem('theme');
+
+// Vérifier si le thème a été sauvegardé précédemment
+if (savedTheme) {
+  body.setAttribute('data-theme', savedTheme);
+  if (savedTheme === 'light') {
+    checkbox.checked = true;
+  }
+} else {
+  body.setAttribute('data-theme', 'dark');
+  localStorage.setItem('theme', 'dark');
+}
+
+// Mettre à jour le thème lorsqu'on coche/décoche la checkbox
+checkbox.addEventListener('change', function() {
+  if (this.checked) {
+    body.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+  } else {
+    body.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  }
 });
