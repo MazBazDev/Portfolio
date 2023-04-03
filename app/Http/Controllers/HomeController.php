3d934a6f065b46182833a67c45044b864376e0a8
MazBaz <mrlog42@gmail.com>
2023-04-04 01:20:13 +0200

    add github readme query

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,43 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller as ControllersController;
+use Parsedown;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller as ControllersController;
 
 class HomeController extends ControllersController
 {
     public function home()
     {
         return view("index", [
-            "repos" => $this->getAllRepos(),
+            "repos" => $this->queryGithubDatas("https://api.github.com/users/" . env('GITHUB_USER_NAME') . "/repos"),
         ]);
     }
 
     public function allRepos() {
         return view("works.index", [
-            "repos" => $this->getAllRepos(),
+            "repos" =>   $this->queryGithubDatas("https://api.github.com/users/" . env('GITHUB_USER_NAME') . "/repos"),
         ]);
     }
 
     function showRepo($id) {
         try {
-            $data = file_get_contents("https://api.github.com/repositories/$id", false, stream_context_create([
-                'http' => [
-                    'header' => [
-                        "User-Agent: PHP",
-                        "Authorization: token " . env("GITHUB_TOKEN"),
-                        "Accept: application/vnd.github.v3+json",
-                    ]
-                ]
-            ]));
-            $repo = json_decode($data);
-        
+            $repo = $this->queryGithubDatas("https://api.github.com/repositories/$id");
             if (isset($repo->message) && $repo->message == "Not Found") {
                 abort(404, 'Page non trouvée');
             }
-        
+            
             return view("works.show", [
-                "repo" => $repo
+                "repo" => $repo,
+                "readme" => $this->getReadmeContent($repo->id),
             ]);
 
         } catch (\Throwable $th) {
@@ -46,26 +38,28 @@ class HomeController extends ControllersController
         }
     }
 
-    private function getAllRepos()
-    {
-        $username = env("GITHUB_USER_NAME");
+    public function changeLocal(Request $request) {
+        setcookie('lang', $request->has("check-lang") ? 'en' : 'fr', time() + (86400 * 30), "/");
 
-        $datas = file_get_contents("https://api.github.com/users/$username/repos", false, stream_context_create([
+        return redirect()->route("home");
+    }
+
+    function getReadmeContent($repoId) {
+        $response = $this->queryGithubDatas("https://api.github.com/repositories/$repoId/readme");
+
+        return Parsedown::instance()->text(base64_decode($response->content));
+    }
+
+    private function queryGithubDatas($url) {
+        $data = file_get_contents($url, false, stream_context_create([
             'http' => [
                 'header' => [
                     "User-Agent: PHP",
                     "Authorization: token " . env("GITHUB_TOKEN"),
+                    "Accept: application/vnd.github.v3+json",
                 ]
             ]
         ]));
-
-        return json_decode($datas);
-    }
-
-    public function changeLocal(Request $request) {
-        
-        setcookie('lang', $request->has("check-lang") ? 'en' : 'fr', time() + (86400 * 30), "/");
-
-        return redirect()->route("home");
+        return json_decode($data);
     }
 }
