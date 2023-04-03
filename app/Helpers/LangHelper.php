266592578fdd93a580415ca4f0bac2afaf599326
MazBaz <mrlog42@gmail.com>
2023-04-03 09:19:27 +0200

    fix default language selector

--- app/Helpers/LangHelper.php
+++ app/Helpers/LangHelper.php
@@ -6,13 +6,13 @@ use Illuminate\Support\Facades\Session;
 if (! function_exists('lang')) {
     function lang($key)
     {
-        return Lang::get($key, [], $_COOKIE['lang']);
+        return Lang::get($key, [], $_COOKIE['lang'] ?? 'fr' );
     }
 }
 
 if (! function_exists('getLocal')) {
     function getLocal()
     {
-        return $_COOKIE['lang'];
+        return $_COOKIE['lang'] ?? "fr";
     }
 }
