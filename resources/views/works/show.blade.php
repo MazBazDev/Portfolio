c5684a8e5abd8f64a5fb34e5d9621ce17777a903
MazBaz <mrlog42@gmail.com>
2023-04-04 09:51:09 +0200

    Added work show page

---
+++ resources/views/works/show.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MazBaz.fr | {{ $repo->name }}</title>
+  <link rel="stylesheet" href="{{ url('/assets/css/style.css') }}">
+
+  <script src="{{ url('/assets/js/jquery-3.6.4.min.js') }}"></script>
+  <script src="https://unpkg.com/scrollreveal"></script>
+</head>
+
+<body data-theme="dark" class="pages">
+    <div class="top">
+        <div class="">
+            <a href="{{ route("works") }}">
+                <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="48" d="M244 400L100 256l144-144M120 256h292"/></svg>
+                back
+            </a>
+        </div>
+
+        <h1>{{ $repo->name }}</h1>
+        
+        <div class="switch-container">
+            <svg xmlns="http://www.w3.org/2000/svg" style="height: 30px; width: 30px;" class="ionicon"
+              viewBox="0 0 512 512">
+              <path
+                d="M160 136c0-30.62 4.51-61.61 16-88C99.57 81.27 48 159.32 48 248c0 119.29 96.71 216 216 216 88.68 0 166.73-51.57 200-128-26.39 11.49-57.38 16-88 16-119.29 0-216-96.71-216-216z"
+                fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" />
+            </svg>
+            <label class="switch">
+              <input type="checkbox" id="check-theme">
+              <span class="slider round"></span>
+            </label>
+            <svg xmlns="http://www.w3.org/2000/svg" style="height: 30px; width: 30px;" class="ionicon"
+              viewBox="0 0 512 512">
+              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="32"
+                d="M256 48v48M256 416v48M403.08 108.92l-33.94 33.94M142.86 369.14l-33.94 33.94M464 256h-48M96 256H48M403.08 403.08l-33.94-33.94M142.86 142.86l-33.94-33.94" />
+              <circle cx="256" cy="256" r="80" fill="none" stroke="currentColor" stroke-linecap="round"
+                stroke-miterlimit="10" stroke-width="32" />
+            </svg>
+        </div>
+    </div>
+      <div class="card">
+        {!! $readme !!}
+      </div>
+      <footer>
+        <span>{{ date("Y") }} MazBaz.fr | made with <span class="hearth">❤️</span> keyboard </span>
+      </footer>
+</body>
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+<script src="{{ url('/assets/js/theme.js') }}"></script>
+</html>
+
